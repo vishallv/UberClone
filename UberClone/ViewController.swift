418ec90c2b5c69eae3f585157d074bd08ac40218
Vishal Lakshmi <vishal.rox26@gmail.com>
2020-01-24 09:32:26 -0800

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Vishal Lakshminarayanappa on 1/24/20.
+//  Copyright © 2020 Vishal Lakshminarayanappa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

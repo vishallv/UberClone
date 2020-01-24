26d7d480f50aed26574014b46f16ccf2b801c413
Vishal Lakshmi <vishal.rox26@gmail.com>
2020-01-24 12:22:02 -0800

    added extension function to anchor the top,left,bottom,right

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -29,9 +29,8 @@ class LoginController: UIViewController{
         view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         
         view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:0).isActive = true
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle{

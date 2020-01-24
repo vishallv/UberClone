4454187468c5fc5a3bf42c22a382677743b063b3
Vishal Lakshmi <vishal.rox26@gmail.com>
2020-01-24 09:57:59 -0800

    Created Project and instantiated the root controller to login controller in sceneDelegate file

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  UberClone
 //
 //  Created by Vishal Lakshminarayanappa on 1/24/20.
 //  Copyright © 2020 Vishal Lakshminarayanappa. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        
+        
     }
-
-
 }
-
